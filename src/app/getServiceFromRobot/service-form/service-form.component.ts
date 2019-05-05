411b1ea2d53704568a6b6aeee60172785f305cdf
Gayan Sampath Manamendra <33378546+GayanSampathManamendra@users.noreply.github.com>
2019-05-05 11:54:14 +0530

    ui updated

--- src/app/getServiceFromRobot/service-form/service-form.component.ts
+++ src/app/getServiceFromRobot/service-form/service-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-service-form',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceFormComponent implements OnInit {
 
+  public availableSlots=["8.00 AM -9.00 AM","9.00 AM - 10 .00 AM","10.00 AM -11 .00 AM"];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  k=0;
+
+  isClick(){
+    if(this.k ==0){
+      this.k=(this.k+1);
+      return this.k;
+    }
+    if(this.k ==1){
+      this.k=(this.k -1);
+      return this.k;
+    }
+  }
+
+  availableTimeSlots(){
+    if(this.k ==1){
+      return true;
+    }
   }
 
 }
--- src/app/socialEventComponents/justGetService/just-get-service-form/just-get-service-form.component.ts
+++ src/app/socialEventComponents/justGetService/just-get-service-form/just-get-service-form.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./just-get-service-form.component.css']
 })
 export class JustGetServiceFormComponent implements OnInit {
+  public availableSlots=["8.00 AM -9.00 AM","9.00 AM - 10 .00 AM","10.00 AM -11 .00 AM"];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  k=0;
+
+  isClick(){
+    if(this.k ==0){
+      this.k=(this.k+1);
+      return this.k;
+    }
+    if(this.k ==1){
+      this.k=(this.k -1);
+      return this.k;
+    }
+  }
+
+  availableTimeSlots(){
+    if(this.k ==1){
+      return true;
+    }
+  }
 }
